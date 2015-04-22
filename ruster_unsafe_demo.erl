6befd120b68a975a0d074c0326a60d3532371db1
Daniel Goertzen <daniel.goertzen@gmail.com>
2015-04-22 14:15:11 -0500

    initial commit

---
+++ ruster_unsafe_demo.erl
@@ -0,0 +1,18 @@
+-module(ruster_unsafe_demo).
+
+-export([static_atom/0,native_add/2, tuple_add/1]).
+-on_load(init/0).
+
+init() ->
+	Filename = hd(filelib:wildcard("target/{debug,release}/libruster_unsafe_demo*")),
+	Rootname = filename:rootname(Filename),
+    ok = erlang:load_nif(Rootname, 0).
+
+static_atom() ->
+    exit(nif_library_not_loaded).
+
+native_add(_X, _Y) ->
+    exit(nif_library_not_loaded).
+
+tuple_add(_X) ->
+    exit(nif_library_not_loaded).
